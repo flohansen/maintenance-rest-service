b9af7a39707b4335e3e8c01e98391d0e531038da
Florian Hansen <flhansen.93@gmail.com>
2021-06-03 14:56:38 +0200

    Implement route to create new users.

--- controllers/user.go
+++ controllers/user.go
@@ -2,9 +2,11 @@ package controllers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/kluddizz/maintenance-rest-service/models"
 )
 
 type (
@@ -22,7 +24,37 @@ func NewUserController(db *sql.DB) *UserController {
 
 // Creates a new user and add it to the database.
 func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var res models.JsonResponse
+	var user models.User
 
+	// Read user from request body
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&user)
+
+	if err != nil {
+		res.Code = 400
+		res.Content = "Could not create new user"
+		res.Send(w)
+		return
+	}
+
+	// Insert the user into the database
+	_, err = uc.Db.Query(
+		"INSERT INTO users (username, password, firstName, lastName, email) VALUES (?, ?, ?, ?, ?)",
+		user.UserName, user.Password, user.FirstName, user.LastName, user.Email,
+	)
+
+	if err != nil {
+		res.Code = 400
+		res.Content = "Internal error"
+		res.Send(w)
+		return
+	}
+
+	// Everything went fine.
+	res.Code = 200
+	res.Content = "Success"
+	res.Send(w)
 }
 
 // Tries to login an user using the given login credentials and send a token if succeeded.
