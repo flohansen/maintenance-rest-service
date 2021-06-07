ae8f3be882404571f6c1dade6bc0b7a65fd536c3
Florian Hansen <flhansen.93@gmail.com>
2021-06-07 15:34:35 +0200

    Optimize and comment test cases.

--- controllers/master_test.go
+++ controllers/master_test.go
@@ -30,30 +30,41 @@ func TestMain(m *testing.M) {
 }
 
 func BeforeAll() {
+  // Read the database config
   dbConfig, err := config.ReadDatabaseConfig("../database.json")
 
   if err != nil {
     panic(err.Error())
   }
 
+  // Try to open a mysql connection
   db, err = sql.Open("mysql", dbConfig.DataSourceName())
 
   if err != nil {
     panic(err.Error())
   }
+
+  // Make sure the database is clean before starting tests.
+  db.Query("DELETE FROM masters")
 }
 
 func AfterAll() {
+  // Remove all generated masters from the database.
   db.Query("DELETE FROM masters")
+
+  // Close the database connection.
   db.Close()
 }
 
+// Test if the route is able to create new masters.
 func TestCreateMaster(t *testing.T) {
+  // Create json string from master object
   jsonStr, err := json.Marshal(master)
   if err != nil {
     t.Fatal("Json marshalling failed")
   }
 
+  // Setup new POST request on /masters
   req, err := http.NewRequest("POST", "http://localhost:3000/masters", bytes.NewBuffer([]byte(jsonStr)))
   req.Header.Set("Content-Type", "application/json")
 
@@ -61,6 +72,7 @@ func TestCreateMaster(t *testing.T) {
     t.Fatal(err.Error())
   }
 
+  // Send the request
   client := &http.Client{}
   res, err := client.Do(req)
 
@@ -70,6 +82,7 @@ func TestCreateMaster(t *testing.T) {
 
   defer res.Body.Close()
 
+  // Read out the inserted (?) master inside the database for validation later.
   var masterDb models.Master
   err = db.QueryRow(
     "SELECT id, name, host, port FROM masters WHERE name = ?",
@@ -82,12 +95,15 @@ func TestCreateMaster(t *testing.T) {
     t.Fatalf("Error in the query: %s", err.Error())
   }
 
+  // Cleanup the database.
   db.Query("DELETE FROM masters")
 
+  // Check if the status code is the expected one.
   if res.StatusCode != 200 {
     t.Errorf("Expected status code to be %d but received %d", 200, res.StatusCode)
   }
 
+  // Check if the inserted master had the right values.
   if masterDb.Name != master.Name ||
      masterDb.Host != master.Host ||
      masterDb.Port != master.Port {
@@ -95,18 +111,23 @@ func TestCreateMaster(t *testing.T) {
   }
 }
 
+// Test if the route is able to delete a valid master from the database and
+// returns a success code.
 func TestDeleteMaster(t *testing.T) {
+  // Insert a new master directly into the dabase.
   queryRes, err := db.Exec(
-    "INSERT INTO masters (id, name, host, port) VALUES (?, ?, ?, ?)",
-    0, master.Name, master.Host, master.Port,
+    "INSERT INTO masters (name, host, port) VALUES (?, ?, ?)",
+    master.Name, master.Host, master.Port,
   )
 
   if err != nil {
     t.Fatalf("Could not insert test master: %s", err.Error())
   }
 
+  // Read out the created id for the master.
   insertedId, _ := queryRes.LastInsertId()
 
+  // Prepare the DELETE request on /masters/:id
   req, err := http.NewRequest(
     "DELETE",
     fmt.Sprintf("http://localhost:3000/masters/%d", insertedId),
@@ -117,6 +138,7 @@ func TestDeleteMaster(t *testing.T) {
     t.Fatal(err.Error())
   }
 
+  // Send the request
   client := &http.Client{}
   res, err := client.Do(req)
 
@@ -124,6 +146,9 @@ func TestDeleteMaster(t *testing.T) {
     t.Fatal(err.Error())
   }
 
+  defer res.Body.Close()
+
+  // Check wheter the master is still present inside the database.
   var masterExists bool
   err = db.QueryRow(
     "SELECT EXISTS(SELECT 1 FROM masters WHERE id = ?)",
@@ -134,20 +159,24 @@ func TestDeleteMaster(t *testing.T) {
     t.Fatal(err.Error())
   }
 
+  // Cleanup the database.
   db.Query("DELETE FROM masters")
 
+  // Check the expected status code.
   if res.StatusCode != 200 {
     t.Errorf("Expected status code to be %d but received %d", 200, res.StatusCode)
   }
 
+  // Check if the master was still present after the DELETE request.
   if masterExists {
     t.Errorf("Expected master existance to be %t but received %t", false, masterExists)
   }
-
-  defer res.Body.Close()
 }
 
+// Test if the route returns an error code when invalid master ids are provided.
 func TestDeleteMasterFail(t *testing.T) {
+  // Prepare the invalid DELETE request on /masters/:id by assuming the id = -1
+  // doesn't exist.
   req, err := http.NewRequest(
     "DELETE",
     fmt.Sprintf("http://localhost:3000/masters/%d", -1),
@@ -158,6 +187,7 @@ func TestDeleteMasterFail(t *testing.T) {
     t.Fatal(err.Error())
   }
 
+  // Send the invalid request.
   client := &http.Client{}
   res, err := client.Do(req)
 
@@ -165,11 +195,12 @@ func TestDeleteMasterFail(t *testing.T) {
     t.Fatal(err.Error())
   }
 
+  defer res.Body.Close()
+
+  // Check if the status code is really an error.
   if res.StatusCode != 400 {
     t.Errorf("Expected status code to be %d but received %d", 400, res.StatusCode)
   }
-
-  defer res.Body.Close()
 }
 
 func TestUpdateMaster(t *testing.T) {
